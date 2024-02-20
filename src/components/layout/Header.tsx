bb65b239eae44933f7c3c494148693fa468ab0d6
Ghunhee Shin <vinocturne@naver.com>
2024-02-20 23:01:15 +0900

    🔨header and responsive

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,41 +1,61 @@
+import { useEffect, useState } from 'react'
+
 export default function Header() {
+  const [isScrolled, setIsScrolled] = useState<boolean>(false)
   const links = [
     { title: 'About Me', to: 'about' },
     { title: 'Tech Stack', to: 'stack' },
     { title: 'Project', to: 'project' },
     { title: 'Career', to: 'career' },
   ]
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY
+      if (scrollPosition > 10) {
+        setIsScrolled(true)
+      } else {
+        setIsScrolled(false)
+      }
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  console.log(isScrolled)
   return (
     <header
-      className={
-        'fixed w-[100%] top-0 right-0 left-0 max-w-[1140px] flex z-[999] px-[12px] py-[10px] justify-between items-center mx-auto bg-transparent'
-      }
+      className={`fixed w-[100%] top-0 right-0 left-0  flex z-[999] px-[12px] py-[10px] justify-center items-center bg-transparent ${isScrolled ? 'bg-[#7D4E57D9]' : ''}`}
     >
-      <div
-        className={'font-extrabold text-2xl text-gray-800 hover:text-yellow-200 cursor-pointer'}
-        onClick={() => {
-          document.getElementById('intro')!.scrollIntoView({ behavior: 'smooth' })
-        }}
-      >
-        Ghunhee&#39;s Portfolio
-      </div>
-      <div className={'font-extrabold text-md flex gap-[12px]'}>
-        {links.map((link) => {
-          return (
-            <a
-              key={link.title}
-              onClick={(event) => {
-                event.preventDefault()
-                document.getElementById(link.to)!.scrollIntoView({
-                  behavior: 'smooth',
-                })
-              }}
-              className={'cursor-pointer text-gray-800 hover:text-yellow-200'}
-            >
-              {link.title}
-            </a>
-          )
-        })}
+      <div className={'max-w-[1140px] w-[100%] flex justify-between items-center'}>
+        <div
+          className={`font-extrabold text-2xl text-gray-800 hover:text-yellow-200 cursor-pointer ${isScrolled ? 'text-white' : ''}`}
+          onClick={() => {
+            document.getElementById('intro')!.scrollIntoView({ behavior: 'smooth' })
+          }}
+        >
+          Ghunhee&#39;s Portfolio
+        </div>
+        <div className={'font-extrabold text-md flex gap-[12px]'}>
+          {links.map((link) => {
+            return (
+              <a
+                key={link.title}
+                onClick={(event) => {
+                  event.preventDefault()
+                  document.getElementById(link.to)!.scrollIntoView({
+                    behavior: 'smooth',
+                  })
+                }}
+                className={`cursor-pointer text-gray-800 hover:text-yellow-200 ${isScrolled ? 'text-white' : ''}`}
+              >
+                {link.title}
+              </a>
+            )
+          })}
+        </div>
       </div>
     </header>
   )
--- src/pages/(root)/components/CareerSection.tsx
+++ src/pages/(root)/components/CareerSection.tsx
@@ -1,3 +1,9 @@
 export default function CareerSection() {
-  return <section id={'career'}></section>
+  return (
+    <section id={'career'} className={'bg-[#364156] py-16'}>
+      <article className={'mx-auto max-w-[1140px] flex flex-col items-center gap-[60px] '}>
+        <span className={'font-han text-5xl text-white'}>Career</span>
+      </article>
+    </section>
+  )
 }
